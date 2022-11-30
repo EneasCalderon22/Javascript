b7e2df6a0f47deab10ab1c30fc5a8ce3a7c1910f
EneasCalderon22 <eneas_calderon@yahoo.com>
2022-11-29 21:29:11 -0300

    Primer proyecto

---
+++ simulador/main.js
@@ -0,0 +1,22 @@
+
+
+let nombre = prompt("Ingrese su nombre")
+
+let apellido = prompt("Ingrese su apellido")
+
+alert (`Hola ${nombre} ${apellido}`)
+
+
+let numero = prompt ("Ingrese su edad")
+
+
+if (numero <= 18){
+    alert("No puede pasar, es menor de edad") 
+ }
+else if (numero > 10){
+    alert ("edad permitida")
+}
+
+
+
+
